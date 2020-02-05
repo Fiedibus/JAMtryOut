727fa89a7e9a55779edd34eeaeb41d1c8493f305
Fiedibus <55784092+Fiedibus@users.noreply.github.com>
2020-02-05 20:38:13 +0100

    Add files via upload

---
+++ Hilfe.java
@@ -0,0 +1,82 @@
+
+public class TEST {
+
+	/**
+	 * SUCHFELD!!!!!!!!!!
+	 */
+//	nameTextField.textProperty().addListener(e->
+//	filterListByName(nameTextField.getText()));
+	
+//	METHODE ZUM SUCHEN!!!!!!!!!!!!
+//	private void filterListByName(String input) {
+//		pokeView.setItems(FXCollections.observableArrayList(pokeList.stream()
+//				.filter(e -> e.getName().toLowerCase().contains(input.toLowerCase())).collect(Collectors.toList())));
+//	}
+	
+	
+	/**
+	 * FILTERN AUFGRUND VON COMBOBOX
+	 */
+//	typeComboBox1=new ComboBox<>();
+//	typeComboBox1.getItems().addAll(Type.values());
+//	typeComboBox1.getSelectionModel().selectFirst();typeComboBox1.valueProperty().addListener(e->
+//	filterListByFirstType(typeComboBox1.getSelectionModel().getSelectedItem()));
+//
+//	METHODE!!!!!!!!!!!!!!!!!!
+//	private void filterListByFirstType(Type input) {
+//		pokeView.setItems(FXCollections.observableArrayList(pokeList.stream()
+//				.filter(e -> e.getFirstType() == input
+//						&& e.getSecondType() == typeComboBox2.getSelectionModel().getSelectedItem())
+//				.collect(Collectors.toList())));
+//	}
+
+		
+	/**
+	 * OBJEKTE IN LISTE EINFÜGEN!!!!!!
+	 */
+//	pokeView=new TableView<Pokemon>(FXCollections.observableArrayList(CSVReader.getPokemonListFromCSV()));
+//	pokeList=CSVReader.getPokemonListFromCSV();
+//	private ObservableList<Article> gamesList = FXCollections.observableArrayList();
+
+	
+	/**
+	 * TABLEVIEW INIT
+	 */
+//	void initTableView() {
+//		TableColumn<Pokemon, String> nameCol = new TableColumn<>("Name");
+//		nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+//		TableColumn<Pokemon, Type> type1Col = new TableColumn<>("Type 1");
+//		type1Col.setCellValueFactory(new PropertyValueFactory<>("firstType"));
+//		TableColumn<Pokemon, Type> type2Col = new TableColumn<>("Type 2");
+//		type2Col.setCellValueFactory(new PropertyValueFactory<>("secondType"));
+//		TableColumn<Pokemon, Integer> totalCol = new TableColumn<>("Total");
+//		totalCol.setCellValueFactory(new PropertyValueFactory<>("total"));
+//		TableColumn<Pokemon, Integer> hpCol = new TableColumn<>("Hp");
+//		hpCol.setCellValueFactory(new PropertyValueFactory<>("hp"));
+//		TableColumn<Pokemon, Integer> attackCol = new TableColumn<>("Attack");
+//		attackCol.setCellValueFactory(new PropertyValueFactory<>("attack"));
+//		TableColumn<Pokemon, Integer> defenseCol = new TableColumn<>("Defense");
+//		defenseCol.setCellValueFactory(new PropertyValueFactory<>("defense"));
+//		TableColumn<Pokemon, Integer> specialAttackCol = new TableColumn<>("Special Attack");
+//		specialAttackCol.setCellValueFactory(new PropertyValueFactory<>("specialAttack"));
+//		TableColumn<Pokemon, Integer> specialDefenseCol = new TableColumn<>("Speciel Defense");
+//		specialDefenseCol.setCellValueFactory(new PropertyValueFactory<>("specialDefense"));
+//		TableColumn<Pokemon, Integer> speedCol = new TableColumn<>("Speed");
+//		speedCol.setCellValueFactory(new PropertyValueFactory<>("speed"));
+//		pokeView.getColumns().addAll(nameCol, type1Col, type2Col, totalCol, hpCol, attackCol, defenseCol,
+//				specialAttackCol, specialDefenseCol, speedCol);
+//	}
+
+	
+	/**
+	 * FÜR ENUMS!!!!!!!!
+	 */
+//	String value;
+//
+//	NAME DER
+//
+//	KLASSE (String value) {
+//		this.value = value;
+//	}
+
+}
